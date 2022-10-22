35cbdbf2af722d8a799babf30fdb5311d8fafbda
kwinnyjevly <kwinnywrites57@gmail.com>
2022-10-22 02:58:22 -0700

    nice

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
-#infendef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int i);
 void print_remaining_days(int month, int day, int year);
