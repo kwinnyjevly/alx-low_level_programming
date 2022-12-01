eb70ad66944e33e8badfe26a9f0824dbbd81c626
kwinnyjevly <kwinnywrites57@gmail.com>
2022-11-30 19:30:31 -0800

    flames

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: A pointer to the head of listint_t list
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
  *
- * Return: number of nodes in the list_t list
+ * Return: numbers of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nnodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
+		nnodes++;
 	}
-
-	return (num);
+	return (nnodes);
 }
