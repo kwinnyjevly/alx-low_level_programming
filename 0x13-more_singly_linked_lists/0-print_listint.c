d03b1c498d4054ae2bfc3aaae39f4f901351c2fa
kwinnyjevly <kwinnywrites57@gmail.com>
2022-11-30 19:56:10 -0800

    cless

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * print_listint - prints a list
+ * @h: the list
  *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	const listint_t *list = h;
+	size_t count = 0;
 
-	while (h)
+	if (!h)
+		return (0);
+
+	while (list)
 	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
+		printf("%d\n", list->n);
+		count++;
+		list = list->next;
 	}
 
-	return (num);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -10,21 +12,33 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
  */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 /**
- * struct listp_s - singly linked list
+ * struct listptr_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
  *
  * Description: singly linked list of pointers
  */
-typedef struct listp_s
+typedef struct listptr_s
 {
 	void *p;
-	struct listp_s *next;
-} listp_t;
+	struct listptr_s *next;
+} listptr_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -38,7 +52,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
-
+size_t free_listint_safe(listint_t **);
+listint_t *find_listint_loop(listint_t *);
+void free_listint(listint_t *);
+void free_listint2(listint_t **);
 #endif
